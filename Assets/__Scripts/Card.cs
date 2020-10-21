8bfd0058d20bcefaa8b301ea410aae0d2cac3469
rpkennedy <55095545+rpkennedy@users.noreply.github.com>
2020-10-21 11:04:10 -0400

    ProsSolit. v1.2
    Draw Pile functional

--- Assets/__Scripts/Card.cs
+++ Assets/__Scripts/Card.cs
@@ -14,11 +14,64 @@ public class Card : MonoBehaviour
     public List<GameObject> pipGOs = new List<GameObject>();
     public GameObject back; 
     public CardDefinition def;
+    public SpriteRenderer[] spriteRenderers;
+
+    void Start()
+    {
+        SetSortOrder(0);  
+    }
+
+    public void PopulateSpriteRenderers()
+    {
+        if (spriteRenderers == null || spriteRenderers.Length == 0)
+        {
+            spriteRenderers = GetComponentsInChildren<SpriteRenderer>();
+        }
+    }
+
+    public void SetSortingLayerName(string tSLN)
+    {
+        PopulateSpriteRenderers();
+        foreach (SpriteRenderer tSR in spriteRenderers)
+        {
+            tSR.sortingLayerName = tSLN;
+        }
+    }
+
+    public void SetSortOrder(int sOrd)
+    {                                
+        PopulateSpriteRenderers();
+        foreach (SpriteRenderer tSR in spriteRenderers)
+        {
+            if (tSR.gameObject == this.gameObject)
+            {
+                tSR.sortingOrder = sOrd; 
+                continue;
+            }
+            switch (tSR.gameObject.name)
+            {
+                case "back": 
+                    tSR.sortingOrder = sOrd + 2;
+                    break;
+
+
+                case "face": 
+                default:     
+                    tSR.sortingOrder = sOrd + 1;
+                    break;
+            }
+        }
+    }
 
     public bool faceUp
     {
         get    { return (!back.activeSelf); }
         set    { back.SetActive(!value); }
+    }
+
+    virtual public void OnMouseUpAsButton()
+    {
+        print(name);
     }
 }
 
